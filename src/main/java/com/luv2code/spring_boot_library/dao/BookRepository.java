6a85a1c92e89a5e79b9b36fae4da22ada87da00a
Samuel <slarico94@gmail.com>
2024-09-25 21:50:50 -0500

    Search book endpoint

--- src/main/java/com/luv2code/spring_boot_library/dao/BookRepository.java
+++ src/main/java/com/luv2code/spring_boot_library/dao/BookRepository.java
@@ -1,11 +1,14 @@
 package com.luv2code.spring_boot_library.dao;
 
 import com.luv2code.spring_boot_library.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
