73cf94cd6132d80d50d8ea9702298a6efd7f7e87
Samuel <slarico94@gmail.com>
2024-10-22 19:58:51 -0500

    review entity and repo

--- src/main/java/com/luv2code/spring_boot_library/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/spring_boot_library/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.luv2code.spring_boot_library.config;
 
 import com.luv2code.spring_boot_library.entity.Book;
+import com.luv2code.spring_boot_library.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -16,9 +17,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.PUT, HttpMethod.DELETE};
 
-        config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Book.class, Review.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         /* Configure CORS */
         cors.addMapping(config.getBasePath() + "/**")
